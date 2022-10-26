88beb9e16a75b198490669f6fb4fa6a473623f02
joao <pjoaovictor22@hotmail.com>
2022-10-26 14:52:32 -0300

    atualizado

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {Details} from './pages/Details'
+import {ThemeProvider} from 'styled-components' //promover um tema
+import theme from "./styles/theme"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Details />
+    <ThemeProvider theme={theme}>
+      <Details />
+    </ThemeProvider>
   </React.StrictMode>
 )
---
+++ src/pages/Details/index.jsx
@@ -0,0 +1,10 @@
+import {Container} from "./styles"
+
+export function Details(){
+  return (
+    <Container>
+      <h1>Hello World!</h1>
+      <samp>ola</samp>
+    </Container>
+  )
+}
