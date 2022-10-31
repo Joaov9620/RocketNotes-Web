fb92b86851e8658fb3a73ea8eaedf1f6e87db973
joao <pjoaovictor22@hotmail.com>
2022-10-31 16:18:36 -0300

    Atualizado

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,4 +1,5 @@
-import { Container, Profile } from "./styles";
+import {RiShutDownLine} from 'react-icons/ri'  //biblioteca de icones
+import { Container, Profile, Logout } from "./styles";
 
 export function Header(){
     return (
@@ -14,6 +15,9 @@ export function Header(){
                     <strong>João Victor Pereira</strong>
                 </div>
             </Profile>
+            <Logout>
+                <RiShutDownLine/>
+            </Logout>
         </Container>
     )
 }
